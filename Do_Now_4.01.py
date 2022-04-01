5018c4358f8c24eae166beb2d7fb630a0f188194
Isabella Feliciano <icfeliciano24@students.bmchs.org>
2022-04-01 09:30:27 -0500

    Lab_4.01.py

--- Do_Now_4.01.py
+++ Do_Now_4.01.py
@@ -12,7 +12,7 @@ Type the following Code
 
 In your notebook
 Respond to the following
-Briefly write down what happened. What would happen if you added 100 items to the list single_fruit? 
+Briefly write down what happened. What would happen if you added 100 items to the list single_fruit? It printed all the fruits in the list. 
 Write down how you would update multi_fruit.
 
 2. In your console
@@ -23,15 +23,25 @@ Type the following
         
 Continue in your notebook
 Respond to the following
-Briefly write down what happened. How would this change if you added 100 items to list_of_numbers?
+Briefly write down what happened. How would this change if you added 100 items to list_of_numbers? It printed all the numbers in the list. 
 
 3. In your console
 Rewrite the code from part 1 using knowledge from part 2.
 
-single_fruit = ['apple', 'banana', 'watermelon', 'grape']
-    multi_fruit = []
-    multi_fruit.append(single_fruit[0] + 's')
-    multi_fruit.append(single_fruit[1] + 's')
-    multi_fruit.append(single_fruit[2] + 's')
-    multi_fruit.append(single_fruit[3] + 's')
-    print(multi_fruit)
+print(single_fruit)
+multi_fruit = []
+for item in single fruit:
+    print(f"{item}s")
+
+for item in single_fruit:
+    multi_fruit.append(f"{item}s")
+print(multi_fruit) 
+
+for item in multi_fruit:
+    print(item)
+
+for i in range(5):
+    print(i)
+
+for i in range(len(multi_fruit)):
+    print(multi_fruit[i])

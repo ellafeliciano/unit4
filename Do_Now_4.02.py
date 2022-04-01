f5a1e0a9a662592e33dd3afd8bc45b9f144282f5
Isabella Feliciano <icfeliciano24@students.bmchs.org>
2022-04-01 09:48:57 -0500

    Do_Now_4.02.py

---
+++ Do_Now_4.02.py
@@ -0,0 +1,24 @@
+Do Now 4.02
+
+1. In your console
+Type the following code
+    for i in range(0, 10):
+        print(i)
+In your notebook
+Respond to the following
+Write down what the range function does. It prints all the numbers from 0-10.
+
+2. In your console
+Type the following
+    a_list = ['apple', 'orange', 'pear', 'strawberry', 'grape']
+    print(len(a_list))
+    print(list(range(0, len(a_list)))
+Continue in your notebook
+Write down what range(0, len(a_list)) does. It names the items in a list from the beginning to the end.
+
+3. In your console
+Use the range and len functions to make a for loop that prints the elements of a_list, one at a time.
+
+a_list = ['apple', 'orange', 'pear', 'strawberry', 'grape']
+for i in range(0, len(a_list)):
+    print(a_list[i])

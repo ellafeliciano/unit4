513013b4f3e29a739493062b8d41ba545f500147
Isabella Feliciano <icfeliciano24@students.bmchs.org>
2022-04-07 09:31:34 -0500

    Do_Now_4.03.py

---
+++ Do_Now_4.03.py
@@ -0,0 +1,56 @@
+'''
+#############
+Do Now 4.03
+#############
+
+Type in your console:
+----------------------
+    def print_6_stars():
+        my_string = ''
+        for i in range(0, 6):
+            my_string += ' *'
+        print(my_string)
+
+In your notebook
+-----------------
+Write down what the output of the function print_6_stars is. It prints * * * * * *
+
+Write a function  print_star_square that calls print_6_stars in a loop to produce the following output.
+
+    >>>python print_stars.py
+    * * * * * *
+    * * * * * *
+    * * * * * *
+    * * * * * *
+    * * * * * *
+    * * * * * *
+
+    # write you code for this function below:
+    def print_star_square():
+        #your code goes here
+    for i in range(6):
+        print_6_stars(6)
+    
+    print_star_square()
+
+Rewrite the function print_star_square without using print_6_stars.
+    # write you code for this function below:
+        def print_star_square():
+            #your code goes here
+
+Note that there are two ways (at least) to get the above output, so just try doing both!
+'''
+def print_6_stars():
+    my_string = ''
+    for i in range(0, 6):
+        my_string += ' *'
+    print(my_string) 
+
+def print_star_square():
+    for i in range(6):
+        my_string = ''
+        for i in range(0, 6):
+            my_string += ' *'
+        print(my_string)
+
+print_star_square()
